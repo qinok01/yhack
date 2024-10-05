3282b573e6a15f3af5ddfc650d472963a2eec927
ecdhyn <109722627+ecdhyn@users.noreply.github.com>
2024-10-05 18:33:00 -0400

    working merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,26 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './tailwind.css';
 import './index.css';
 import PersonalTrainerLanding from './components/PersonalTrainingLanding';
-<<<<<<< HEAD
 import Fitness from './components/Fitness/Fitness';
 
-const App = () => {
-  return (
-    <div>
-      <Fitness/>
-    </div>
-=======
-import FitnessRoom from './components/FitnessRoom(depreceated)';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<PersonalTrainerLanding />} />
-        <Route path="/fitness" element={<FitnessRoom />} />
+        <Route path="/fitness" element={<Fitness />} />
       </Routes>
     </Router>
->>>>>>> f131c22e673ed8a5943b2b52517e02d2dc6b5f51
   );
 };
 
